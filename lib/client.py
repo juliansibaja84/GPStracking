5cae923674061727384a9fcb509a246b5ffb6e9e
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-08-29 03:55:15 -0500

    Testing listener service

--- lib/client.py
+++ lib/client.py
@@ -4,8 +4,8 @@ import sys
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 9000)
-message = '>REV051910172634+1098989-0748271600000012;ID=ENOMOTO<\r\n'
+server_address = ('ennen.org', 9000)
+message = '>REV051910172658+1101979-0748510800000012;ID=ENOMOTO<\r\n'
 
 try:
     sent = sock.sendto(bytes(message, 'UTF-8'), server_address)
