939d7e6c939ffbedaed3f0c5b905cb3c6ef1eb15
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-09-06 21:48:03 -0500

    Fixed django versions compativility problem

--- firstsite/historic/views.py
+++ firstsite/historic/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.template import loader
+import os
+import sqlite3
 
 
 def index(request):
