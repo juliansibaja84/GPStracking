ee9aa5927f67e4d13a353506eedc474fec16fe5c
Julián Sibaja <juliansibaja84@gmail.com>
2016-09-11 23:28:40 -0500

    position on any place of the map

--- lib/client.py
+++ lib/client.py
@@ -4,13 +4,8 @@ import sys
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-<<<<<<< HEAD
-server_address = ('localhost', 9000)
-message = '>REV051910172626+1099006-0748269000000012;ID=ENOMOTO<\r\n'
-=======
-server_address = ('ennen.org', 9000)
-message = '>REV051910172632+1099000-0748272100000012;ID=ENOMOTO<\r\n'
->>>>>>> 939d7e6c939ffbedaed3f0c5b905cb3c6ef1eb15
+server_address = ('enomoto.sytes.net', 9000)
+message = '>REV051910172628+1099014-0748270400000012;ID=ENOMOTO<\r\n'
 
 try:
     sent = sock.sendto(bytes(message, 'UTF-8'), server_address)
