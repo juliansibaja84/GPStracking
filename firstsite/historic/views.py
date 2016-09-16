d2921d098743e7d5fe6bd74a89959b9d61e7e60e
Julián Sibaja <juliansibaja84@gmail.com>
2016-09-15 22:52:34 -0500

    locate position and time by click on a given position and time range[WORKING]

--- firstsite/historic/views.py
+++ firstsite/historic/views.py
@@ -27,14 +27,42 @@ def loadElementsT(lower, upper, latitude, longitude):
     com1 = lower.replace('T', ' ')
     com2 = upper.replace('T', ' ')
     a = 0.0022
-    LimLonLow = str(float(longitude)-a)
-    LimLonHigh = str(float(longitude)+a)
-    LimLatLow = str(float(latitude)-a)
-    LimLatHigh = str(float(latitude)+a)
+    limLatLow = str(float(latitude)-a)
+    limLatHigh = str(float(latitude)+a)
+    limLonLow = str(float(longitude)-a)
+    limLonHigh = str(float(longitude)+a)
+    limits = [limLatLow, limLatHigh, limLonLow, limLonHigh]
+    lim_temp = 0
+    if float(latitude) < 0:
+        lim_temp = limits[0]
+        limits[0] = limits[1]
+        limits[1] = lim_temp
+    if float(longitude) < 0:
+        lim_temp = limits[2]
+        limits[2] = limits[3]
+        limits[3] = lim_temp
+
+    lim=['0','0','0','0']; a = 0
+    for limit in limits:
+        if float(limit) >= 0:
+            if float(limit)%10 < 1:
+                lim[a] = "+0"+limit
+            else:
+                lim[a] = "+"+limit       
+        else:
+            if abs(float(limit))%10 < 1:
+                lim[a] = "-00"+str((-1)*float(limit))
+            elif abs(float(limit))%10 < 10:
+                lim[a] = "-0"+str((-1)*float(limit))
+            else:
+                lim[a] = str(limit)
+        a += 1
     conn, cc = createConnectionAndCursor()
-    dat = cc.execute("select * from (SELECT * FROM log WHERE tiempo BETWEEN '" + com1 + "' AND '" + com2 + "') as T WHERE latitud BETWEEN '"+LimLatLow+"' AND '"+LimLatHigh+"' and longitud BETWEEN '"+LimLonLow+"' AND '"+LimLonHigh + "'")
+    dat = cc.execute("SELECT * FROM (SELECT * FROM log WHERE tiempo BETWEEN '" + com1 + "' AND '" + com2 + "') as T WHERE latitud BETWEEN '"+lim[0]+"' AND '"+lim[1]+"' and longitud BETWEEN '"+lim[2]+"' AND '"+lim[3] + "'")
     dat = dat.fetchall()
     return constructDictionary(dat)
+
+    
 
 
 def loadElements(lower, upper):
