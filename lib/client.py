01dbc7ff32e40cd6d0f092cc9cd853fb17097531
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-10-11 13:49:15 -0500

    App android

--- lib/client.py
+++ lib/client.py
@@ -7,9 +7,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = ('ennen.org', 9000)
 
-tim = 60140
+tim = 60150
 lat = 1099001
-lon = 7482145
+lon = 7482150
 
 m = '>REV0519101' + str(tim) + '+' + str(lat) + '-0' + str(lon) + '00000012;ID=ENOMOTO<\r\n'
 try:
