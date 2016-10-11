366436804882ff02057df47675e46030580dc25d
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-10-11 14:44:36 -0500

    df

--- app/Misaka/app/src/main/java/org/ennen/misaka/Samsara.java
+++ app/Misaka/app/src/main/java/org/ennen/misaka/Samsara.java
@@ -2,6 +2,8 @@ package org.ennen.misaka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Samsara extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Samsara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.samsara);
+    }
+
+    public void onClickbutton(View view) {
+        Toast.makeText(this, "Holi", Toast.LENGTH_SHORT).show();
     }
 }
