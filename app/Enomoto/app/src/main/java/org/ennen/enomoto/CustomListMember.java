f6dca0f79bb4088dd7bd490542979f2e3cff716a
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-10-14 16:54:42 -0500

    Great advance app

---
+++ app/Enomoto/app/src/main/java/org/ennen/enomoto/CustomListMember.java
@@ -0,0 +1,96 @@
+package org.ennen.enomoto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by asmateus on 14/10/16.
+ */
+
+public class CustomListMember extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<>();
+    private Context context;
+
+    public CustomListMember(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+
+        // Hide delete button on start
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0;
+        //return list.get(pos).getId();
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_item, null);
+        }
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+        listItemText.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        ImageButton deleteBtn = (ImageButton)view.findViewById(R.id.delete_btn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                list.remove(position); //or some other task
+                notifyDataSetChanged();
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toogleButton(v);
+            }
+        });
+
+        return view;
+    }
+
+    public void toogleButton(View v) {
+        ImageButton btn = (ImageButton)v.findViewById(R.id.delete_btn);
+        if(btn.getVisibility() == View.GONE) {
+            btn.setClickable(true);
+            btn.setVisibility(View.VISIBLE);
+        }
+        else {
+            btn.setClickable(false);
+            btn.setVisibility(View.GONE);
+        }
+    }
+}
--- app/Enomoto/app/src/main/java/org/ennen/enomoto/MainActivity.java
+++ app/Enomoto/app/src/main/java/org/ennen/enomoto/MainActivity.java
@@ -1,7 +1,12 @@
 package org.ennen.enomoto;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -9,17 +14,55 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> elements_to_record = new ArrayList<>();
+    private int info_bar_status = 0;
+    private Snackbar info;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.task_selector);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        ArrayList<String> elements_to_record = new ArrayList<>();
-        elements_to_record.add(0, "hola");
-        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X", "Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, mobileArray);
+        elements_to_record.add("Android");
+        elements_to_record.add("IPhone");
+        elements_to_record.add("WindowsMobile");
+        elements_to_record.add("Blackberry");
+        elements_to_record.add("WebOS");
+        elements_to_record.add("Ubuntu");
+        elements_to_record.add("Windows7");
+        elements_to_record.add("Max OS X");
+        elements_to_record.add("Arch Linux");
+        elements_to_record.add("Elementary OS");
+        elements_to_record.add("Gentoo");
+        elements_to_record.add("Nginx");
+        elements_to_record.add("Django");
+        elements_to_record.add("Tensor Flow");
+        elements_to_record.add("Python");
+        elements_to_record.add("Haskell");
+        elements_to_record.add("Ennen");
+        elements_to_record.add("Trovador");
+
+        CustomListMember adapter = new CustomListMember(elements_to_record, this);
 
         ListView listView = (ListView) findViewById(R.id.tracking_list);
         listView.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(info_bar_status == 0) {
+                    info = Snackbar.make(view, "No OBD-II adapter Found, please check Bluetooth connection", Snackbar.LENGTH_INDEFINITE);
+                    info.setAction("Action", null).show();
+                    info_bar_status = 1;
+                }
+                else {
+                    info.setAction("Action", null).dismiss();
+                    info_bar_status = 0;
+                }
+            }
+        });
     }
 }
